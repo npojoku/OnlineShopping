3fc6a5d6db71f39c1f571eaa7f5890dd0d4b2d57
Hongyi Li <hongyili93@gmail.com>
2017-03-26 22:06:29 -0700

    try to get rid of DS_Store

--- backend/product/searchByProductName.php
+++ backend/product/searchByProductName.php
@@ -30,6 +30,26 @@ Error:
 	-7						required field not set
 */
 
+if (isset($_GET['searchText']) && !empty($_GET['searchText'])) {
+	$searchText = $_GET['searchText'];
+	
+	$query = "SELECT ProductName, ProductDescription, COUNT(OrderId) AS NumRating, AVG(Rating) AS AverageRating FROM `Products`, Rating WHERE Products.ProductId = Rating.ProductId AND Products.ProductId = '$ProductId'";
+	$result = mysqli_query($con, $query);
+	$resultArray = array();
+	while($r = mysqli_fetch_array($result)) {
 
+		$one['ProductName']= $r['ProductName'];
+		$one['ProductDescription'] = $r['ProductDescription'];
+		$one['NumRating']= $r['NumRating'];
+		$one['AverageRating']= $r['AverageRating'];
+
+    	$resultArray[] = $one;
+	}
+
+	print json_encode($resultArray);
+
+} else {
+	echo "-7";
+}
 
 ?>
