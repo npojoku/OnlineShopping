bc93c63654388704d7b7a38a3758460e607c200e
Theresa Mammarella <theresamammarella@Theresas-MacBook-Pro.local>
2017-03-26 22:32:22 -0700

    start of profile page

--- frontend/php/parts/profileBody.php
+++ frontend/php/parts/profileBody.php
@@ -1,1 +1,82 @@
-This is the profile page.
+<!-- Page Content -->
+<div class="container">
+
+    <div class="row">
+
+        <div class="col-md-9">
+
+            <div class="thumbnail">
+                <div class="caption-full" style="
+                        padding-left: 140px;
+                        padding-right: 140px;
+                        padding-top: 25px;
+                        padding-bottom: 25px;">
+                    <h3>Account Settings
+                    </h3>
+                   <form class="form-horizontal">
+                      <div class="form-group">
+                        <label class="col-sm-2 control-label">Email</label>
+                        <div class="col-sm-10">
+                          <input type="Email" class="form-control" id="inputEmail" placeholder="Email">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label for="inputPassword" class="col-sm-2 control-label">Password</label>
+                        <div class="col-sm-10">
+                          <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+                        </div>
+                      </div>
+                    <div class="form-group">
+                        <label for="inputFirstName" class="col-sm-2 control-label" style=" padding-left: 0px; padding-right: 0px;">First Name</label>
+                        <div class="col-sm-10">
+                          <input type="text" class="form-control" id="inputFirstName" placeholder="First Name">
+                        </div>
+                      </div>
+                    <div class="form-group">
+                        <label for="inputLastName" class="col-sm-2 control-label" style=" padding-left: 0px; padding-right: 0px;">Last Name</label>
+                        <div class="col-sm-10">
+                          <input type="text" class="form-control" id="inputLastName" placeholder="Last Name">
+                        </div>
+                      </div>
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">Phone</label>
+                        <div class="col-sm-10">
+                          <input type="text" class="form-control" id="inputPhone" placeholder="Phone">
+                        </div>
+                      </div>
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">Address</label>
+                        <div class="col-sm-10">
+                          <input type="text" class="form-control" id="inputAddress" placeholder="Address">
+                        </div>
+                      </div>
+                    </form>
+
+                    <form class="form-horizontal">
+                    	<div class="row">
+                              <table class="table table-bordered table-hover" id="tableAddRow">
+                                  <thead>
+                                      <tr>
+                                          <th>Credit Card #</th>
+                                          <th>CreditExpDate</th>
+                                          <th style="width:10px"><span class="glyphicon glyphicon-plus addBtn" id="addBtn_0"></span></th>
+                                      </tr>
+                                  </thead>
+                                  <tbody>
+                                      <tr id="tr_0">
+                                          <td><input type="text" id="creditCard#" class="form-control"/></td>
+                                          <td><input type="text" id="creditExpDate" class="form-control" /></td>
+                                          <td><span class="glyphicon glyphicon-minus addBtnRemove" id="addBtnRemove_0"></span></td>
+                                      </tr>
+                                  </tbody>
+                              </table>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</div>
