40ebcc882abc34713e2b51dc34eb3c00ac3429be
Theresa Mammarella <theresamammarella@dhcp-128-189-78-34.ubcsecure.wireless.ubc.ca>
2017-03-29 11:10:39 -0700

    connected login to backend, updated register links

--- backend/login/logIn.php
+++ backend/login/logIn.php
@@ -18,48 +18,64 @@ Error:
 	-7						required field not set
 */
 global $con;
-
 $errors = array();
 
 // verify database connection
 if (!$con) {
+  // could not connect to database, redirect to error page
   header("Location: ../../frontend/php/error.php");
   exit;
-} else {
+} else if(isset($_POST['login'])){
+  $hasErrror = false;
 
-  if(isset($_POST['login'])){
-      echo 'hello I got to here';
+  // verify email has been entered
+  if(!isset($_POST['Email']) || empty($_POST['Email'])){
+    $errors[] = '<div class="alert alert-danger" role="alert"><center>Please include a valid email.</center></div>';
+    $hasError = true;
   }
 
-  if (isset($_POST['Email']) && isset($_POST['Password'])) {
-  	global $con;
-  	$Email = $_POST['Email'];
-  	$Password = $_POST['Password'];
-  	$Password_hash = md5($Password);
-
-  	if (!empty($Email) && !empty($Password)) {
-  		$sql = "SELECT PersonId FROM Person WHERE Email='$Email' AND Password = '$Password_hash'";
-
-  		$result = $con->query($sql);
-
-  		if ($result->num_rows > 0) {
-  			$PersonId = $result->fetch_assoc()['PersonId'];
-  			$_SESSION['PersonId'] = $PersonId;
-
-  			$sql2 = "SELECT PersonId FROM Retailers WHERE PersonId='$PersonId'";
-  			$result2 = $con->query($sql2);
-
-  			if ($result2->num_rows > 0) {
-  				$_SESSION['UserType'] = '1';
-  				echo "11";
-  			}else {
-  				$_SESSION['UserType'] = '0';
-  				echo "10";
-  			}
-  		} else {
-  			echo "0";
-  		}
-  	}
+  // verify password has been entered
+  if(!isset($_POST['Password']) || empty($_POST['Password'])){
+    $errors[] = '<div class="alert alert-danger" role="alert"><center>Please include a valid password.</center></div>';
+    $hasError = true;
   }
+
+  // don't continue if fields are not valid
+  if($hasError){
+    exit;
+  }
+
+  // process login request
+	$Email = $_POST['Email'];
+	$Password = $_POST['Password'];
+	$Password_hash = md5($Password);
+
+	$sql = "SELECT PersonId FROM Person WHERE Email='$Email' AND Password = '$Password_hash'";
+
+	$result = $con->query($sql);
+
+  // login was successful
+	if ($result->num_rows > 0) {
+    // store session
+		$PersonId = $result->fetch_assoc()['PersonId'];
+		$_SESSION['PersonId'] = $PersonId;
+
+    // assign user type
+		$sql2 = "SELECT PersonId FROM Retailers WHERE PersonId='$PersonId'";
+		$result2 = $con->query($sql2);
+
+		if ($result2->num_rows > 0) {
+			$_SESSION['UserType'] = '1';
+		}else {
+			$_SESSION['UserType'] = '0';
+		}
+
+    // forward to product list page
+    header("Location: ../../frontend/php/productList.php");
+
+	} else {
+    // login was unsuccessful
+    $errors[] = '<div class="alert alert-danger" role="alert"><center>Login was unsuccessful.</center></div>';
+	}
 }
 ?>
