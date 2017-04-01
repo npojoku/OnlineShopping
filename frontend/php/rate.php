f40967b816ddeb58cdb38d4f2e3d3afb4ffd0271
Kevin Zhu <Kevin Zhu>
2017-03-31 19:48:25 -0700

    rating part

---
+++ frontend/php/rate.php
@@ -0,0 +1,21 @@
+<DOCTYPE html>
+
+<!-- user session must be logged in to view this page -->
+<?php include '../../backend/session.php'; ?>
+
+<html>
+<!-- header -->
+<?php include('parts/head.php'); ?>
+
+<body>
+<!-- navigation bar -->
+<?php include('parts/nav.php'); ?>
+
+<!-- body -->
+<?php include('parts/rateBody.php'); ?>
+
+<!-- footer -->
+<?php include('parts/footer.php'); ?>
+
+</body>
+</html>
