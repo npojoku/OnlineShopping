4d454f2866024923119de9300e5bf8d84b85a552
Hongyi Li <hongyili93@gmail.com>
2017-03-31 22:20:50 -0700

    auto add rating 0 to db when new product is made

--- backend/product/addProduct.php
+++ backend/product/addProduct.php
@@ -16,8 +16,21 @@ if(isset($_POST['addProduct'])) {
 
       // create product and redirect
       if(createProduct($con, $ProductName, $ProductDescription)){
-        // return to manage products page
-        header("Location: ../../frontend/php/createSells.php");
+        $sql1 = "SELECT ProductId FROM Products WHERE ProductName = '$ProductName'";
+        if($result=mysqli_query($con,$sql1)){
+          $pida = mysqli_fetch_array($result, MYSQLI_ASSOC);
+          $pid = $pida['ProductId'];
+          $sql2 = "INSERT INTO Rating(ProductId, Rating, OrderId) VALUES($pid, 0, 0)";
+          $query2 = mysqli_prepare($con, $sql2); 
+          if($query2->execute()){
+            header("Location: ../createShells.php");
+          } else {
+            // if query failed go to error page
+            header("Location: ../../frontend/php/error.php");
+          }
+        } else {
+          header("Location: ../../frontend/php/error.php");
+        }    
       } else {
         // if query failed go to error page
         header("Location: ../../frontend/php/error.php");
@@ -31,11 +44,12 @@ if(isset($_POST['addProduct'])) {
 function createProduct($con, $ProductName, $ProductDescription){
   $PersonId = getPersonId();
   $sql = "INSERT INTO Products(ProductName, ProductDescription)
-    VALUES ('$ProductName','$ProductDescription')";
+  VALUES ('$ProductName','$ProductDescription')";
 
   $query = mysqli_prepare($con, $sql);
 
   return $query->execute();
 }
+
 
 ?>
