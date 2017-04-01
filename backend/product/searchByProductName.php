395f31dd2fc102502c212a88fb20b1ef71a6ac13
Theresa Mammarella <theresamammarella@Theresas-MacBook-Pro.local>
2017-03-31 21:31:19 -0700

    debug fix

--- backend/product/searchByProductName.php
+++ backend/product/searchByProductName.php
@@ -1,38 +1,10 @@
 <?php
 require '../core.inc.php';
 require '../session.php';
-/*
-Path: ---------------------------
-
-Description: search product by its Name or description if searchText empty, view all
-Input:
-		$_GET
-			'searchText'		(String) 
-Output
-		json array:
-			[ProductId]				ProductId
-			[ProductName]			ProductName
-			[ImageURL]				Product image url
-			[UsedQuanity]			UsedQuanity
-			[NewQuanity]			NewQuanity
-			[AverageRating]			Average rating for the product
-			[NewPrice]				NewPrice
-			[UsedPrice]				UsedPrice
-
-Error:
-	0						failed
-	-1						no session
-	-2						field must not be empty/null
-	--						----------
-	--						----------
-	--						----------
-	--						----------
-	-7						required field not set
-*/
 
 if (isset($_GET['searchText']) && !empty($_GET['searchText'])) {
 	$searchText = $_GET['searchText'];
-	
+
 	$query = "SELECT ProductName, ProductDescription, COUNT(OrderId) AS NumRating, AVG(Rating) AS AverageRating FROM `Products`, Rating WHERE Products.ProductId = Rating.ProductId AND Products.ProductId = '$ProductId'";
 	$result = mysqli_query($con, $query);
 	$resultArray = array();
@@ -49,7 +21,7 @@ if (isset($_GET['searchText']) && !empty($_GET['searchText'])) {
 	print json_encode($resultArray);
 
 } else {
-	echo "-7";
+	header("Location: ../../frontend/php/login.php");
 }
 
 ?>
